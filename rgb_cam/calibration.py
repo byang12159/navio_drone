571be0b6a1bcdd9c2aac3f1a0533398eebf75314
byang12159 <92831530+byang12159@users.noreply.github.com>
2024-01-18 14:08:35 -0600

    add vicon logger

---
+++ rgb_cam/calibration.py
@@ -0,0 +1,95 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import glob
+import cv2
+import pickle
+
+
+def Calibrate():
+
+    
+    # Arrays to store 3D points and 2D image points
+    obj_points = []
+    img_points = []
+    
+    # Prepare expected object 3D object points (0,0,0), (1,0,0) ...
+    object_points = np.zeros((7*7,3), np.float32)
+    object_points[:,:2] = np.mgrid[0:7, 0:7].T.reshape(-1,2)
+    object_points = object_points*square_size
+    
+    fnames = glob.glob('/path/to/folder/pi_sensor_setup/camera_calibration/images/'+'*.'+'jpg')
+    
+    
+    for fname in fnames:
+        img = cv2.imread(fname)
+        img_size = (img.shape[1], img.shape[0])
+        gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Find chess board corners
+        ret, corners = cv2.findChessboardCorners(gray_scale, (num_intersections_in_x, num_intersections_in_y), None)
+        
+        if ret:
+            obj_points.append(object_points)
+            img_points.append(corners)
+            
+            # Draw the corners
+            drawn_img = cv2.drawChessboardCorners(img, (7,7), corners, ret)
+            # Uncomment the below code (43-47) if you want to visualize the detected corners
+            # drawn_img = cv2.resize(drawn_img, (500,500))
+            # cv2.namedWindow("main", cv2.WINDOW_NORMAL)
+            # cv2.imshow("main", drawn_img)
+            # cv2.waitKey(0)
+
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, img_size, None, None)
+
+    img = cv2.imread(fnames[19])
+    dst = cv2.undistort(img, mtx, dist, None, mtx)
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+    ax1.imshow(img)
+    ax1.set_title('Original Image', fontsize=15)
+    ax2.imshow(dst)
+    ax2.set_title('Undistorted Image', fontsize=15)
+    plt.show()
+
+    return [ret, mtx, dist, rvecs, tvecs]
+
+def capture_photos(num_photos, interval_sec):
+    with picamera.PiCamera() as camera:
+        for i in range(num_photos):
+            # Capture a photo
+            photo_name = f"photo_{i + 1}.jpg"
+            camera.capture(photo_name)
+            print(f"Captured {photo_name}")
+
+            # Wait for the specified interval
+            time.sleep(interval_sec)
+
+if __name__ == "__main__":
+
+    import picamera
+    import time
+
+    # Picture Capture Params:
+    # Number of consecutive photos to capture
+    num_photos = 5
+    # Interval between consecutive photos (in seconds)
+    interval_sec = 5
+
+    # Calibration Params:
+    # Number of object points
+    num_intersections_in_x = 7
+    num_intersections_in_y = 7
+    
+    # Size of square in meters
+    square_size = 0.0225
+
+    capture_photos(num_photos, interval_sec)
+
+    ret, mtx, dist, rvecs, tvecs = Calibrate()
+
+    # Code used to store varaibles, Inntrinsic params as mtx, and distortion coeficient as dist. Uncomment the below lines to store them again.
+    dist_pickle = {}
+    dist_pickle["mtx"] = mtx
+    dist_pickle["dist"] = dist
+    pickle.dump(dist_pickle, open("dist_pickle.p", "wb"))
---
+++ scripts/vicon_logger.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+import rospy
+from pymavlink import mavutil
+import sys, os
+import numpy as np
+from std_msgs.msg import Float64MultiArray
+import pickle
+import argparse
+
+parser = argparse.ArgumentParser(description="")
+parser.add_argument('--save',type=str, help='filename to save the data.')
+args = parser.parse_args()
+
+def vlog():
+    rospy.init_node('vicon_bridge', anonymous=True)
+    pub = rospy.Publisher('/vicon_estimate', Float64MultiArray, queue_size=1)
+
+    # create a mavlink serial instance
+    master = mavutil.mavlink_connection('udpin:0.0.0.0:10086')
+
+    data = Float64MultiArray()
+
+    data.data = [0, ] * (9 + 2 + 4 + 1)
+
+    datastorage = []
+
+    while not rospy.is_shutdown():
+        msg = master.recv_match(blocking=False)
+        if not msg:
+            continue
+
+        if msg.get_type() == 'LOCAL_POSITION_NED_COV':
+            data.data[0] = msg.x / 1000.
+            data.data[1] = msg.y / 1000.
+            data.data[2] = msg.z / 1000.
+            data.data[3] = msg.vx / 1000.
+            data.data[4] = msg.vy / 1000.
+            data.data[5] = msg.vz / 1000.
+            data.data[6] = msg.ax / 1000.
+            data.data[7] = msg.ay / 1000.
+            data.data[8] = msg.az / 1000.
+
+            # print("Coordinaes recieved")
+            # use msg.covaricane to store the yaw and yaw_rate, and q
+            offset = 100.
+            data.data[9] = msg.covariance[0] - offset
+            data.data[10] = msg.covariance[1] - offset
+
+            data.data[11] = msg.covariance[2] - offset
+            data.data[12] = msg.covariance[3] - offset
+            data.data[13] = msg.covariance[4] - offset
+            data.data[14] = msg.covariance[5] - offset
+
+            now = rospy.get_rostime()
+            now = now.to_sec()
+            data.data[-1] = now
+            pub.publish(data)
+
+            datastorage.append(data)
+
+        elif msg.get_type() == 'ATT_POS_MOCAP':
+            pass
+    
+    return datastorage
+
+if __name__ == '__main__':
+    datastorage = vlog()
+
+    if args.save is not None:
+        with open('f{args.save}.pkl','wb') as file:
+            pickle.dump(datastorage,file)
+        print(f"saved data to {args.save}")
+    else:
+        print("finished vicon without saving")
